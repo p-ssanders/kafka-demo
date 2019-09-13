6b7b685d677d2a724fc3616e7ec1e5e906ef88e9
Sam Sanders <ssanders@pivotal.io>
2019-09-13 17:55:01 -0600

    ensure tests pass without kafka running

--- src/test/java/com/example/kafkademo/KafkaDemoApplicationTests.java
+++ src/test/java/com/example/kafkademo/KafkaDemoApplicationTests.java
@@ -3,10 +3,14 @@ package com.example.kafkademo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EmbeddedKafka
+@TestPropertySource("classpath:test.properties")
 public class KafkaDemoApplicationTests {
 
   @Test
